5ac23d1131f50a4dc0796d2945650b8d4eb5be24
sabyasachi <njoysubho@gmail.com>
2022-01-26 22:13:27 +0100

    Update Micronaut and other deps

--- src/main/kotlin/org/api/Application.kt
+++ src/main/kotlin/org/api/Application.kt
@@ -1,10 +1,22 @@
 package org.api
 
 import io.micronaut.runtime.Micronaut
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Info
 
-fun main(args: Array<String>) {
-    Micronaut.build()
+@OpenAPIDefinition(
+    info = Info(
+        title = "Goal",
+        version = "0.0.1",
+        description = "Goal and Category API"
+    )
+)
+object Application {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        Micronaut.build()
             .args(*args)
             .packages("org.api")
             .start()
+    }
 }
